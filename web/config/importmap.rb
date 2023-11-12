b775dcb144246471a8e6af6978d0bef25469ac32
said.montufar <said.montufar@outlook.com>
2023-11-11 21:40:14 -0500

    Test branch

--- web/config/importmap.rb
+++ web/config/importmap.rb
@@ -5,3 +5,6 @@
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin_all_from "app/javascript/lib", under: "lib"
+pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.1.1/app/assets/javascripts/rails-ujs.esm.js"
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js"
